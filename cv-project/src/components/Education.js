acc4b146cff31864856ac050d516b4939d5413c1
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-17 15:34:10 -0500

    Add HTML Structure

--- cv-project/src/components/Education.js
+++ cv-project/src/components/Education.js
@@ -1,7 +1,48 @@
 const Education = () => {
     return (
         <section>
-            <div></div>
+            <div className='title'>
+                <h3>Education</h3>
+            </div>
+            <form>
+                <div className='form-pair'>
+                    <div className='form-input'>
+                        <label htmlFor='school'>School Name</label>
+                        <input type='text' id='school'></input>
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='location'>Location</label>
+                        <input type='text' id='location'></input>
+                    </div>
+                </div>
+                <div className='form-pair'>
+                    <div className='form-input'>
+                        <label htmlFor='degree'>Degree</label>
+                        <input type='text' id='degree'></input>
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='field'>Field Of Study</label>
+                        <input type='text' id='field'></input>
+                    </div>
+                </div>
+                <div className='form-pair'>
+                    <div className='form-input'>
+                        <label htmlFor='start'>Start Date</label>
+                        <input type='text' id='start'></input>
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='end'>Graduation Date</label>
+                        <input type='tel' id='end'></input>
+                    </div>
+                </div>
+                <div className='form-input'>
+                    <input type='checkbox' id='check'></input>
+                    <label htmlFor='check'>I currently attend here</label>
+                </div>
+                <p>+ Add another degree</p>
+                <button type='submit'>Back</button>
+                <button type='submit'>Save and continue</button>
+            </form>
         </section>
     );
 };
