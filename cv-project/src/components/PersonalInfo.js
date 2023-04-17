0ce274d3cc4a01a6ea0c5e0c109f397cd171e633
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-17 15:13:38 -0500

    Edit class names

--- cv-project/src/components/PersonalInfo.js
+++ cv-project/src/components/PersonalInfo.js
@@ -6,40 +6,40 @@ const PersonalInfo = () => {
             </div>
             <form>
                 <div className='form-pair'>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='fName'>First Name</label>
                         <input type='text' id='fName'></input>
                     </div>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='lName'>Last Name</label>
                         <input type='text' id='lName'></input>
                     </div>
                 </div>
-                <div className='formInput'>
+                <div className='form-input'>
                     <label htmlFor='profession'>Profession</label>
                     <input type='text' id='profession'></input>
                 </div>
                 <div className='form-pair'>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='city'>City/State</label>
                         <input type='text' id='city'></input>
                     </div>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='postal'>Postal Code</label>
                         <input type='text' id='postal'></input>
                     </div>
                 </div>
                 <div className='form-pair'>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='Country'>Country</label>
                         <input type='text' id='country'></input>
                     </div>
-                    <div className='formInput'>
+                    <div className='form-input'>
                         <label htmlFor='pnone'>Phone</label>
                         <input type='tel' id='phone'></input>
                     </div>
                 </div>
-                <div className='formInput'>
+                <div className='form-input'>
                     <label htmlFor='email'>Email</label>
                     <input type='email' id='email'></input>
                 </div>
