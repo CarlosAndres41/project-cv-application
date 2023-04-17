d30fd881e2271b03afc497d99cb658ebc834851c
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-16 22:40:26 -0500

    Add logo and name

--- cv-project/src/components/Header.js
+++ cv-project/src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Header = () => {
     return (
         <header>
-            <h1>Hello World</h1>
+            <div className='logo-name'>
+                <FontAwesomeIcon
+                    icon={icon({ name: 'id-card', size: '2xl' })}
+                    className='logo'
+                />
+                <h1 className='name'>CV Generator</h1>
+            </div>
         </header>
     );
 };
