ff8a50b9780012ad22c8bd5273d6425eca279b34
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-16 21:40:59 -0500

    Create Class App. Add 'components' folder.

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,7 +1,14 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-    return <div></div>;
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>Hello World!</div>;
+    }
 }
 
 export default App;
