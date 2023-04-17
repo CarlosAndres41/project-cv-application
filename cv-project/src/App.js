7322cff6b2c9fe86d49633a641414615078e65d2
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-16 21:55:47 -0500

    Create basic header component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     }
 
     render() {
-        return <div>Hello World!</div>;
+        return <Header />;
     }
 }
 
