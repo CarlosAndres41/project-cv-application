72a44f8efba5ff1964d0d8dbdbfeaeb18f5ea238
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-18 00:15:12 -0500

    Add onChange event handler to all inputs

--- cv-project/src/components/PersonalInfo.js
+++ cv-project/src/components/PersonalInfo.js
@@ -1,4 +1,4 @@
-const PersonalInfo = () => {
+const PersonalInfo = (props) => {
     return (
         <section>
             <div className='title'>
@@ -8,40 +8,72 @@ const PersonalInfo = () => {
                 <div className='form-pair'>
                     <div className='form-input'>
                         <label htmlFor='fName'>First Name</label>
-                        <input type='text' id='fName'></input>
+                        <input
+                            type='text'
+                            id='fName'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                     <div className='form-input'>
                         <label htmlFor='lName'>Last Name</label>
-                        <input type='text' id='lName'></input>
+                        <input
+                            type='text'
+                            id='lName'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                 </div>
                 <div className='form-input'>
                     <label htmlFor='profession'>Profession</label>
-                    <input type='text' id='profession'></input>
+                    <input
+                        type='text'
+                        id='profession'
+                        onChange={props.handler}
+                    ></input>
                 </div>
                 <div className='form-pair'>
                     <div className='form-input'>
                         <label htmlFor='city'>City/State</label>
-                        <input type='text' id='city'></input>
+                        <input
+                            type='text'
+                            id='city'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                     <div className='form-input'>
                         <label htmlFor='postal'>Postal Code</label>
-                        <input type='text' id='postal'></input>
+                        <input
+                            type='text'
+                            id='postal'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                 </div>
                 <div className='form-pair'>
                     <div className='form-input'>
                         <label htmlFor='Country'>Country</label>
-                        <input type='text' id='country'></input>
+                        <input
+                            type='text'
+                            id='country'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                     <div className='form-input'>
                         <label htmlFor='phone'>Phone</label>
-                        <input type='tel' id='phone'></input>
+                        <input
+                            type='tel'
+                            id='phone'
+                            onChange={props.handler}
+                        ></input>
                     </div>
                 </div>
                 <div className='form-input'>
                     <label htmlFor='email'>Email</label>
-                    <input type='email' id='email'></input>
+                    <input
+                        type='email'
+                        id='email'
+                        onChange={props.handler}
+                    ></input>
                 </div>
                 <button type='submit'>Save and continue</button>
             </form>
