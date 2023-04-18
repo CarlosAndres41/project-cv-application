3099a236cf4aad6c9f07dff23e8a1f0407adbda9
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-18 15:07:52 -0500

    Create Preview component

---
+++ cv-project/src/components/Preview.js
@@ -0,0 +1,10 @@
+const Preview = (props) => {
+    return (
+        <section className='preview'>
+            <div className='personal-info'></div>
+            <div className='main-body'></div>
+        </section>
+    );
+};
+
+export default Preview;
