5576ee20cf3bcfcd148dfcfb5c075154d6780563
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-18 16:11:16 -0500

    Change key value for summary

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
                 end: '',
             },
             summary: {
-                desc: '',
+                summary: '',
             },
         };
     }
