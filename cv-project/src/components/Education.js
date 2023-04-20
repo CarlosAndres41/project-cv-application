224afdd746edcff666a51484b04829b76062b5d3
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-19 22:51:00 -0500

    Display education form depending of the number of items in count

--- cv-project/src/components/Education.js
+++ cv-project/src/components/Education.js
@@ -4,87 +4,89 @@ const Education = (props) => {
             <div className='title'>
                 <h3>Education</h3>
             </div>
-            <form>
-                <div className='form-pair'>
-                    <div className='form-input'>
-                        <label htmlFor='school'>School Name</label>
-                        <input
-                            type='text'
-                            id='school'
-                            onChange={props.handler}
-                        ></input>
+            {props.count.map((item, i) => (
+                <form key={i}>
+                    <div className='form-pair'>
+                        <div className='form-input'>
+                            <label htmlFor='school'>School Name</label>
+                            <input
+                                type='text'
+                                id='school'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
+                        <div className='form-input'>
+                            <label htmlFor='location'>Location</label>
+                            <input
+                                type='text'
+                                id='location'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
                     </div>
-                    <div className='form-input'>
-                        <label htmlFor='location'>Location</label>
-                        <input
-                            type='text'
-                            id='location'
-                            onChange={props.handler}
-                        ></input>
+                    <div className='form-pair'>
+                        <div className='form-input'>
+                            <label htmlFor='degree'>Degree</label>
+                            <input
+                                type='text'
+                                id='degree'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
+                        <div className='form-input'>
+                            <label htmlFor='field'>Field Of Study</label>
+                            <input
+                                type='text'
+                                id='field'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
                     </div>
-                </div>
-                <div className='form-pair'>
-                    <div className='form-input'>
-                        <label htmlFor='degree'>Degree</label>
-                        <input
-                            type='text'
-                            id='degree'
-                            onChange={props.handler}
-                        ></input>
+                    <div className='form-pair'>
+                        <div className='form-input'>
+                            <label htmlFor='start'>Start Date</label>
+                            <input
+                                type='date'
+                                id='start'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
+                        <div className='form-input'>
+                            <label htmlFor='end'>Graduation Date</label>
+                            <input
+                                type='date'
+                                id='end'
+                                onChange={props.handler}
+                            ></input>
+                        </div>
                     </div>
-                    <div className='form-input'>
-                        <label htmlFor='field'>Field Of Study</label>
-                        <input
-                            type='text'
-                            id='field'
-                            onChange={props.handler}
-                        ></input>
+                    <div className='form-input checkbox'>
+                        <input type='checkbox' id='check'></input>
+                        <label htmlFor='check'>I currently attend here</label>
                     </div>
-                </div>
-                <div className='form-pair'>
-                    <div className='form-input'>
-                        <label htmlFor='start'>Start Date</label>
-                        <input
-                            type='date'
-                            id='start'
-                            onChange={props.handler}
-                        ></input>
-                    </div>
-                    <div className='form-input'>
-                        <label htmlFor='end'>Graduation Date</label>
-                        <input
-                            type='date'
-                            id='end'
-                            onChange={props.handler}
-                        ></input>
-                    </div>
-                </div>
-                <div className='form-input checkbox'>
-                    <input type='checkbox' id='check'></input>
-                    <label htmlFor='check'>I currently attend here</label>
-                </div>
-                <p className='add' data-current='education' onClick={props.add}>
-                    + Add another degree
-                </p>
-                <div className='buttons'>
-                    <button
-                        type='submit'
-                        className='back'
-                        onClick={props.back}
-                        data-current='education'
-                    >
-                        Back
-                    </button>
-                    <button
-                        type='submit'
-                        className='next'
-                        onClick={props.next}
-                        data-current='education'
-                    >
-                        Next: summary
-                    </button>
-                </div>
-            </form>
+                </form>
+            ))}
+            <p className='add' data-current='education' onClick={props.add}>
+                + Add another degree
+            </p>
+            <div className='buttons'>
+                <button
+                    type='submit'
+                    className='back'
+                    onClick={props.back}
+                    data-current='education'
+                >
+                    Back
+                </button>
+                <button
+                    type='submit'
+                    className='next'
+                    onClick={props.next}
+                    data-current='education'
+                >
+                    Next: summary
+                </button>
+            </div>
         </section>
     );
 };
